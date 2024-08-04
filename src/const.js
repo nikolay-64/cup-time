5b5592d2dedf37185fc318eff4ad6faeb3366688
nikolay-64 <kolyavyazmin@yandex.ru>
2024-08-04 15:23:44 +0400

    the product category was displayed in the heading Products

--- src/const.js
+++ src/const.js
@@ -1,3 +1,9 @@
 export const API_URL = 'https://cup-time-api-o3kp.onrender.com';
 // /api/products/{category}
-// /api/products/{title}
+export const productTitles = [
+	{ namecategory: 'tea', nametitle: 'Чай' },
+	{ namecategory: 'coffee', nametitle: 'Кофе' },
+	{ namecategory: 'teapots', nametitle: 'Чайники' },
+	{ namecategory: 'cezves', nametitle: 'Турки' },
+	{ namecategory: 'other', nametitle: 'Прочее' },
+];
--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -3,20 +3,26 @@ import { Product } from './Product';
 import { useProducts } from '../context/ProductContext';
 import { useSearchParams } from 'react-router-dom';
 
+import { productTitles } from '../const';
+import { useState } from 'react';
+
 export const Products = () => {
 	const [seachParams] = useSearchParams();
 	const { products, setCategory } = useProducts();
 	const category = seachParams.get('category');
+	const [title, setTitle] = useState('');
 
 	useEffect(() => {
+		const productsTitle = productTitles.find(item => item.namecategory 
+			=== category);
 		setCategory(category);
+		setTitle(productsTitle.nametitle);
 	}, [category, setCategory]);
 
 	return (
 		<section className="products">
 			<div className="container">
-				<h2 className="products__title">Чай</h2>
-
+				<h2 className="products__title">{title}</h2>
 				<ul className="products__list">
 					{products.map((item) => (
 						<Product key={item.id} data={item} />
